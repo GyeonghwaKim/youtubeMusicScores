a8b53945e2ed90a8dfbc34dc7c1655c9da1d7343
GyeongHwa <gyeonghwaa_@naver.com>
2024-05-27 18:30:30 +0900

    delete preSecurity

--- src/main/java/com/example/youtubeSheet/preSecurity/EncryptHelper.java
+++
@@ -1,8 +0,0 @@
-package com.example.youtubeSheet.preSecurity;
-
-public interface EncryptHelper {
-
-    String encrypt(String password);
-
-    Boolean isMatch(String password,String hashPassword);
-}
